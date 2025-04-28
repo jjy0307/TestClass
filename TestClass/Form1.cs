050b52c9ae56db37707e82d829c098d1e6ad0663
jjy0307 <adz7546@gmail.com>
2025-04-28 09:57:14 +0900

    add show text in testBox

--- TestClass/Form1.cs
+++ TestClass/Form1.cs
@@ -21,6 +21,14 @@ namespace TestClass
             float floatNum = 10.5f;
             double doubleNum = 10.55d;
             decimal decimalNum = 10.555m;
+
+
+            testBox_print.Text += byteNum.GetType() + " byteNum  " + byteNum.ToString() + "\r\n";
+            testBox_print.Text += shortNum.GetType() + " shortNum : " + shortNum.ToString() + "\r\n";
+            testBox_print.Text += intNum.GetType() + " intNum : " + intNum.ToString() + "\r\n";
+            testBox_print.Text += floatNum.GetType() + " floatNum : " + floatNum.ToString() + "\r\n";
+            testBox_print.Text += doubleNum.GetType() + " doubleNum : " + doubleNum.ToString() + "\r\n";
+            testBox_print.Text += decimalNum.GetType() + " decimalNum : " + decimalNum.ToString() + "\r\n";
         }
     }
 }

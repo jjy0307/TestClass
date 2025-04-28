f1799fb2763d866e429272e9bc4900b920341a7c
jjy0307 <adz7546@gmail.com>
2025-04-28 09:53:43 +0900

    mod textBox Name

--- TestClass/Form1.Designer.cs
+++ TestClass/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.testBox_print = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -42,20 +42,20 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Result :";
             // 
-            // textBox1
+            // testBox_print
             // 
-            this.textBox1.Location = new System.Drawing.Point(154, 122);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(477, 274);
-            this.textBox1.TabIndex = 1;
+            this.testBox_print.Location = new System.Drawing.Point(154, 122);
+            this.testBox_print.Multiline = true;
+            this.testBox_print.Name = "testBox_print";
+            this.testBox_print.Size = new System.Drawing.Size(477, 274);
+            this.testBox_print.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.testBox_print);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,7 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox testBox_print;
     }
 }
 

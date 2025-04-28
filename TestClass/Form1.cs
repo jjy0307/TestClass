c14fc67f59c0af193154ae28ed6259a831bc929b
jjy0307 <adz7546@gmail.com>
2025-04-28 10:03:21 +0900

    Mod text

--- TestClass/Form1.cs
+++ TestClass/Form1.cs
@@ -23,12 +23,12 @@ namespace TestClass
             decimal decimalNum = 10.555m;
 
 
-            testBox_print.Text += byteNum.GetType() + " byteNum  " + byteNum.ToString() + "\r\n";
-            testBox_print.Text += shortNum.GetType() + " shortNum : " + shortNum.ToString() + "\r\n";
-            testBox_print.Text += intNum.GetType() + " intNum : " + intNum.ToString() + "\r\n";
-            testBox_print.Text += floatNum.GetType() + " floatNum : " + floatNum.ToString() + "\r\n";
-            testBox_print.Text += doubleNum.GetType() + " doubleNum : " + doubleNum.ToString() + "\r\n";
-            testBox_print.Text += decimalNum.GetType() + " decimalNum : " + decimalNum.ToString() + "\r\n";
+            testBox_print.Text += byteNum.GetType() + " byteNum: " + byteNum.ToString() + "\r\n";
+            testBox_print.Text += shortNum.GetType() + " shortNum: " + shortNum.ToString() + "\r\n";
+            testBox_print.Text += intNum.GetType() + " intNum: " + intNum.ToString() + "\r\n";
+            testBox_print.Text += floatNum.GetType() + " floatNum: " + floatNum.ToString() + "\r\n";
+            testBox_print.Text += doubleNum.GetType() + " doubleNum: " + doubleNum.ToString() + "\r\n";
+            testBox_print.Text += decimalNum.GetType() + " decimalNum: " + decimalNum.ToString() + "\r\n";
         }
     }
 }
